cc0dce3bf0c29f495caf9524c4ddbef2461958da
Colin Scott <colin@abstractcode.com>
2013-10-09 11:36:42 +0800

    Add ISettingsProvider abstraction to support multiple sources.

---
+++ CoderMike.Autofac.EasySettings/ISettingsProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoderMike.Autofac.EasySettings
+{
+	public interface ISettingsProvider
+	{
+		IEnumerable<string> AllKeys { get; }
+		string this[string key] { get; }
+	}
+}
--- CoderMike.Autofac.EasySettings/SimpleSettingsReader.cs
+++ CoderMike.Autofac.EasySettings/SimpleSettingsReader.cs
@@ -1,39 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Collections.Specialized;
+using System.Linq;
 
 namespace CoderMike.Autofac.EasySettings
 {
-    public class SimpleSettingsReader : ISettingsReader
-    {
-        private readonly NameValueCollection _settingsSource;
+	public class SimpleSettingsReader : ISettingsReader
+	{
+		private readonly ISettingsProvider _settingsProvider;
 
-        public SimpleSettingsReader(NameValueCollection settingsSource)
-        {
-            if (settingsSource == null)
-                throw new ArgumentNullException("settingsSource");
-            _settingsSource = settingsSource;
-        }
+		public SimpleSettingsReader(NameValueCollection settingsSource)
+		{
+			if (settingsSource == null)
+			{
+				throw new ArgumentNullException("settingsSource");
+			}
+			_settingsProvider = new NameValueCollectionSettingsProvider(settingsSource);
+		}
 
-        public object Read(Type settingsType)
-        {
-            if (settingsType == null)
-                throw new ArgumentNullException("settingsType");
-            var settingsObj = Activator.CreateInstance(settingsType);
-            var settingsPrefix = settingsType.Name.Replace("Settings", "") + ":";
-            foreach (var key in _settingsSource.AllKeys.Where(x => x.StartsWith(settingsPrefix)))
-            {
-                var propertyName = key.Substring(settingsPrefix.Length);
-                var property = settingsType.GetProperty(propertyName);
-                if (property == null)
-                    throw new Exception(String.Format("Settings class {0} has no property called {1}", settingsType.Name, propertyName));
+		public object Read(Type settingsType)
+		{
+			if (settingsType == null)
+			{
+				throw new ArgumentNullException("settingsType");
+			}
+			var settingsObj = Activator.CreateInstance(settingsType);
+			var settingsPrefix = settingsType.Name.Replace("Settings", "") + ":";
+			foreach (var key in _settingsProvider.AllKeys.Where(x => x.StartsWith(settingsPrefix)))
+			{
+				var propertyName = key.Substring(settingsPrefix.Length);
+				var property = settingsType.GetProperty(propertyName);
+				if (property == null)
+				{
+					throw new Exception(String.Format("Settings class {0} has no property called {1}", settingsType.Name, propertyName));
+				}
 
-                var settingValue = Convert.ChangeType(_settingsSource[key], property.PropertyType);
-                property.SetValue(settingsObj, settingValue, null);
-            }
-            return settingsObj;
-        }
-    }
+				var settingValue = Convert.ChangeType(_settingsProvider[key], property.PropertyType);
+				property.SetValue(settingsObj, settingValue, null);
+			}
+			return settingsObj;
+		}
+	}
 }
