0daa8165e60e621602e3178d657bc1b96b200661
Colin Scott <colin@abstractcode.com>
2013-10-10 17:51:12 +0800

    Provide ability to specify file or ISettingsProvider to Autofac module.

---
+++ CoderMike.Autofac.EasySettings.Tests/AutofacConfiguration/FileSetupFixture.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+
+using Autofac;
+
+using CoderMike.Autofac.EasySettings.Tests.SettingsReading;
+
+using Xunit;
+
+namespace CoderMike.Autofac.EasySettings.Tests.AutofacConfiguration
+{
+	public class FileSetupFixture
+	{
+		[Fact]
+		public void ModuleInstallsSimpleSettingsReaderByDefault()
+		{
+			var module = new EasySettingsModule("MultipleEntries.json");
+
+			var builder = new ContainerBuilder();
+			builder.RegisterModule(module);
+
+			var container = builder.Build();
+			var reader = container.Resolve<ISettingsReader>();
+
+			Assert.NotNull(reader);
+			Assert.IsType<SimpleSettingsReader>(reader);
+		}
+
+		[Fact]
+		public void ModuleInstallsSettingsSource()
+		{
+			var module = new EasySettingsModule("MultipleEntries.json");
+
+			var builder = new ContainerBuilder();
+			builder.RegisterModule(module);
+
+			var container = builder.Build();
+			Assert.NotEmpty(container.ComponentRegistry.Sources.OfType<SettingsSource>());
+		}
+
+		[Fact]
+		public void ConfigurationValuesLoadedFromSuppliedCollection()
+		{
+			var module = new EasySettingsModule("MultipleEntries.json");
+
+			var builder = new ContainerBuilder();
+			builder.RegisterModule(module);
+
+			var container = builder.Build();
+			var reader = container.Resolve<ISettingsReader>();
+
+			var instance = reader.Read<FirstSettings>();
+
+			Assert.Equal("blah", instance.Value);
+		}
+	}
+}
